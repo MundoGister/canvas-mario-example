022b9280b16b70f493d28f9baf61faf064929365
Julio García Pérez <juliojose.garcia@11paths.com>
2018-05-24 20:46:31 +0200

    add base game class

--- js/game.js
+++ js/game.js
@@ -1,14 +1,34 @@
 function Game(canvasElement) {
+  this.ctx = canvasElement.getContext("2d");
+
+  this.background = new Background(this.ctx);
+  this.player = new Player(this.ctx);
+
+  this.intervalId = null;
 }
 
 Game.prototype.start = function() {
+  this.intervalId = setInterval(function() {
+    this.clear();
+    this.draw();
+    this.moveAll();
+  }, 16);
+};
+
+Game.prototype.stop = function() {
+  clearInterval(this.intervalId);
 };
 
 Game.prototype.draw = function() {
+  this.background.draw();
+  this.player.draw();
 };
 
 Game.prototype.moveAll = function() {
+  this.background().move();
+  this.player().move();
 };
 
 Game.prototype.clear = function() {
+  this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
 };
